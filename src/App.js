938dd24eb83790a78f498a6964ed842ab540b207
Adam Rasheed <adamrasheed91@gmail.com>
2020-06-03 21:15:36 -0700

    reverted to biolerplate emptiness

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-import Layout from './components/Layout/index';
 
 function App() {
   return (
-    <Router>
-
-      <Layout />
-    </Router>
+    <div>test</div>
   );
 }
 
